df2a1f031957526d5149c16cb58222cd471b101a
cykoravish <ravishbisht85@gmail.com>
2026-02-03 16:42:09 +0530

    mail-send-after-payment

---
+++ backend/helpers/helper.js
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: process.env.SMTP_SECURE === "true", // true for 465, false for 587
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export async function sendOwnerEmail({ subject, html }) {
+    console.log(process.env.SMTP_HOST)
+  await transporter.sendMail({
+    from: `"Regenta Payments" <${process.env.MAIL_FROM}>`,
+    to: process.env.OWNER_EMAIL,
+    subject,
+    html,
+  });
+};
